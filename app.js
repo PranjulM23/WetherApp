e111181c3ea97debe6c2f5fff336e311148e0cb2
PranjullM23 <pranjulmangal0987@gmail.com>
2022-07-29 11:52:28 +0530

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 app.set("view engine" , "hbs")
-app.use('',(req,res)=>{
+app.use('/',(req,res)=>{
     res.render("index")
 })
 app.get('/', (req, res) => res.send('Hello World!'))
